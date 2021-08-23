5c98c5c6e6aa7aeaaacf7880f6a169e9b06d667b
Paul Hein <pauldhein@email.arizona.edu>
2021-08-23 15:16:29 -0400

    Finished first draft definition of CHIME SIR vs SVIIVR GIG def

--- scripts/gromet/example_gig_CHIME_SIR_dyn_only-CHIME_SVIIvR_dyn_only.py
+++ scripts/gromet/example_gig_CHIME_SIR_dyn_only-CHIME_SVIIvR_dyn_only.py
@@ -6,70 +6,275 @@ import gromet_intersection_graph as ig
 # -----------------------------------------------------------------------------
 
 
-def generate_gig() -> GrometIntersectionGraph:
+def generate_gig() -> ig.GrometIntersectionGraph:
+    chime_sir_name = "CHIME_SIR"
+    chime_sviivr_name = "CHIME_SVIIvR"
+    ver_chime_sir_name = f"{chime_sir_name}_v01"
+    ver_chime_sviivr_name = f"{chime_sviivr_name}_v01"
+    gig_uid_str = f"{ver_chime_sir_name}<->{ver_chime_sviivr_name}"
 
     # TODO: implement correct nodes based off of this guide
     common_nodes = [
         ig.CommonNode(
             uid=ig.UidCommonNode("S_in"),
             # type='input',
-            g1_variable=[ig.UidVariable("S")],
-            g2_variable=[ig.UidVariable("V:sir.s_in")],
+            g1_variable=[ig.UidVariable("V:sir.s_in")],
+            g2_variable=[ig.UidVariable("V:sviivr.s_in")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("I_in"),
+            # type='input',
+            g1_variable=[ig.UidVariable("V:sir.i_in")],
+            g2_variable=[ig.UidVariable("V:sviivr.i_in")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("R_in"),
+            # type='input',
+            g1_variable=[ig.UidVariable("V:sir.r_in")],
+            g2_variable=[ig.UidVariable("V:sviivr.r_in")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("beta"),
+            # type='input',
+            g1_variable=[ig.UidVariable("V:simsir_loop_1.beta")],
+            g2_variable=[ig.UidVariable("V:simsviivr_loop_1.beta")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("gamma"),
+            # type='input',
+            g1_variable=[ig.UidVariable("V:main.gamma")],
+            g2_variable=[ig.UidVariable("V:main.gamma")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("n"),
+            # type='input',
+            g1_variable=[ig.UidVariable("V:simsir_loop_1.n")],
+            g2_variable=[ig.UidVariable("V:simsviivr_loop_1.n")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("scale"),
+            # type='internal',
+            g1_variable=[ig.UidVariable("V:sir.scale")],
+            g2_variable=[ig.UidVariable("V:sviivr.scale")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("S_n"),
+            # type='internal',
+            g1_variable=[ig.UidVariable("V:sir.s_n")],
+            g2_variable=[ig.UidVariable("V:sviivr.s_n")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("I_n"),
+            # type='internal',
+            g1_variable=[ig.UidVariable("V:sir.i_n")],
+            g2_variable=[ig.UidVariable("V:sviivr.i_n")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("R_n"),
+            # type='internal',
+            g1_variable=[ig.UidVariable("V:sir.r_n")],
+            g2_variable=[ig.UidVariable("V:sviivr.r_n")],
         ),
         ig.CommonNode(
             uid=ig.UidCommonNode("S_out"),
             # type='output',
-            g1_variable=[ig.UidVariable("S_2")],
-            g2_variable=[ig.UidVariable("V:sir.s_out")],
+            g1_variable=[ig.UidVariable("V:sir.s_out")],
+            g2_variable=[ig.UidVariable("V:sviivr.s_out")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("I_out"),
+            # type='output',
+            g1_variable=[ig.UidVariable("V:sir.i_out")],
+            g2_variable=[ig.UidVariable("V:sviivr.i_out")],
+        ),
+        ig.CommonNode(
+            uid=ig.UidCommonNode("R_out"),
+            # type='output',
+            g1_variable=[ig.UidVariable("V:sir.r_out")],
+            g2_variable=[ig.UidVariable("V:sviivr.r_out")],
         ),
     ]
 
-    # TODO: implement correct nodes based off of this guide
-    oap_nodes = [
-        ig.OAPNode(
-            uid=ig.UidCommonNode("OAP_1"),
-            g1_variables=[
-                ig.UidVariable("infected"),
-                ig.UidVariable("recovered"),
-                ig.UidVariable("dt"),
+    oap_nodes = []
+
+    noap_nodes = [
+        ig.NOAPNode(
+            uid=ig.UidCommonNode("NOAP_1"),
+            gromet_name=ver_chime_sviivr_name,
+            variables=[
+                ig.UidVariable("V:main.v_r"),
+                ig.UidVariable("V:main.v_s"),
+                ig.UidVariable("V:simsviivr_loop_1.v"),
+                ig.UidVariable("V:simsviivr_loop_1.i_v"),
             ],
-            g2_variables=[
-                ig.UidVariable("V:sir.n"),
-                ig.UidVariable("V:sir.s_n"),
-                ig.UidVariable("V:sir.i_n"),
-                ig.UidVariable("V:sir.r_n"),
-                ig.UidVariable("V:sir.scale"),
+        ),
+        ig.NOAPNode(
+            uid=ig.UidCommonNode("NOAP_2"),
+            gromet_name=ver_chime_sviivr_name,
+            variables=[
+                ig.UidVariable("V:sviivr.v_n"),
+                ig.UidVariable("V:sviivr.i_v_n"),
+            ],
+        ),
+        ig.NOAPNode(
+            uid=ig.UidCommonNode("NOAP_3"),
+            gromet_name=ver_chime_sviivr_name,
+            variables=[
+                ig.UidVariable("V:sviivr.v"),
+                ig.UidVariable("V:sviivr.i_v"),
             ],
         ),
     ]
 
     edges = [
         ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("n"),
+            dst=ig.UidCommonNode("scale"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("beta"),
+            dst=ig.UidCommonNode("S_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("beta"),
+            dst=ig.UidCommonNode("I_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("S_in"),
+            dst=ig.UidCommonNode("S_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("S_in"),
+            dst=ig.UidCommonNode("I_n"),
+        ),
+        ig.Edge(
             type="no_overlap",
             src=ig.UidCommonNode("S_in"),
-            dst=ig.UidCommonNode("OAP_1"),
+            dst=ig.UidCommonNode("NOAP_2"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("I_in"),
+            dst=ig.UidCommonNode("S_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("I_in"),
+            dst=ig.UidCommonNode("I_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("I_in"),
+            dst=ig.UidCommonNode("R_n"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("I_in"),
+            dst=ig.UidCommonNode("NOAP_2"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("R_in"),
+            dst=ig.UidCommonNode("R_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("gamma"),
+            dst=ig.UidCommonNode("I_n"),
+        ),
+        ig.Edge(
+            type="g1_subset_g2",
+            src=ig.UidCommonNode("gamma"),
+            dst=ig.UidCommonNode("R_n"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("gamma"),
+            dst=ig.UidCommonNode("NOAP_2"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("NOAP_1"),
+            dst=ig.UidCommonNode("NOAP_2"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("NOAP_1"),
+            dst=ig.UidCommonNode("S_n"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("NOAP_1"),
+            dst=ig.UidCommonNode("I_n"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("NOAP_1"),
+            dst=ig.UidCommonNode("R_n"),
+        ),
+        ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("scale"),
+            dst=ig.UidCommonNode("S_out"),
+        ),
+        ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("scale"),
+            dst=ig.UidCommonNode("I_out"),
+        ),
+        ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("scale"),
+            dst=ig.UidCommonNode("R_out"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("scale"),
+            dst=ig.UidCommonNode("NOAP_3"),
+        ),
+        ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("S_n"),
+            dst=ig.UidCommonNode("S_out"),
+        ),
+        ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("I_n"),
+            dst=ig.UidCommonNode("I_out"),
+        ),
+        ig.Edge(
+            type="equal",
+            src=ig.UidCommonNode("R_n"),
+            dst=ig.UidCommonNode("R_out"),
+        ),
+        ig.Edge(
+            type="no_overlap",
+            src=ig.UidCommonNode("NOAP_2"),
+            dst=ig.UidCommonNode("NOAP_3"),
         ),
     ]
 
     # TODO: Check the attrs of this object
     gromet_ids = ig.GrometIds(
-        g1_name="CHIME_SIR_v01",
-        g1_uid=ig.UidGromet("CHIME_SIR"),
-        g2_name="CHIME_SVIIvR_v01",
-        g2_uid=ig.UidGromet("CHIME_SVIIvR"),
+        g1_name=ver_chime_sir_name,
+        g1_uid=ig.UidGromet(chime_sir_name),
+        g2_name=ver_chime_sviivr_name,
+        g2_uid=ig.UidGromet(chime_sviivr_name),
     )
 
-    # TODO: Check the attrs of this object
-    gig = ig.GrometIntersectionGraph(
-        uid=ig.UidIntersectionGraph("GIG.CHIME_SIR_v01<->CHIME_SVIIvR_v01"),
+    return ig.GrometIntersectionGraph(
+        uid=ig.UidIntersectionGraph(gig_uid_str),
         gromet_ids=gromet_ids,
         common_nodes=common_nodes,
         oap_nodes=oap_nodes,
-        noap_nodes=None,
+        noap_nodes=noap_nodes,
         edges=edges,
     )
-
-    return gig
 
 
 # -----------------------------------------------------------------------------
